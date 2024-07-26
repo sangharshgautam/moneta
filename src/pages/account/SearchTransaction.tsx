1a30d09779d1033f65ce1531d22e89135fe0481f
sangharsh <sangharsh_iitd@yahoo.com>
2024-07-26 18:29:23 +0100

    Search Transactions

--- src/pages/account/SearchTransaction.tsx
+++ src/pages/account/SearchTransaction.tsx
@@ -1,13 +1,12 @@
 import Transactions from "../transaction/Transactions";
 import React, {useEffect, useState} from "react";
-import {Account, Agency, Transaction} from "../../components/modules/common/Models";
+import {Account, Transaction} from "../../components/modules/common/Models";
 import MonetaApi from "../../services/MonetaApi";
 
 const SearchTransaction = (props: {account: Account}) => {
-    const [progress, setProgress] = useState(0)
     const [transactions, setTransactions] = useState<Transaction[]>([])
     useEffect(() => {
-        MonetaApi.search<Transaction[]>(`account/${props.account.id}/transaction`, {}, setProgress).then(
+        MonetaApi.search<Transaction[]>(`account/${props.account.id}/transaction`, {}).then(
             result => setTransactions(result.data)
         )
     }, [props.account.id])
