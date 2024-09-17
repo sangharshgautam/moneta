42aeb4757a5721e0a418e70b8e38415e24dddaf4
sangharsh <sangharsh_iitd@yahoo.com>
2024-09-17 18:10:23 +0100

    unused imports

--- src/pages/report/ViewReport.tsx
+++ src/pages/report/ViewReport.tsx
@@ -43,20 +43,15 @@ const ViewReport = () => {
         { menuItem: 'Tax', render: () => <TabPane><CorpTaxReportView report={report}></CorpTaxReportView></TabPane> },
     ]
     return <React.Suspense fallback={<OutletContentLoading resource={'account'} />}>
-        {/*
-        // @ts-ignore */}
-        <Await resolve={useLoaderData().itemResponse} errorElement={<OutletContentError />}>{(itemResponse) => (
-            <>
-                <ViewItemSection resource="account"/>
-                <TxnSearchForm startDate={txnFilters.startDate} endDate={txnFilters.endDate} handleSubmit={handleSubmit}></TxnSearchForm>
-                {/*<IncomeReport report={report}></IncomeReport>*/}
-                <TxnChart report={report}></TxnChart>
-                <Segment basic>
-                    <Tab panes={panes} />
-                </Segment>
-            </>
-        )}
-        </Await>
+        <>
+            <ViewItemSection resource="account"/>
+            <TxnSearchForm startDate={txnFilters.startDate} endDate={txnFilters.endDate} handleSubmit={handleSubmit}></TxnSearchForm>
+            {/*<IncomeReport report={report}></IncomeReport>*/}
+            <TxnChart report={report}></TxnChart>
+            <Segment basic>
+                <Tab panes={panes} />
+            </Segment>
+        </>
     </React.Suspense>
 }
 export default ViewReport
